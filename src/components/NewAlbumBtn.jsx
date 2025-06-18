f27a5c7c95e61e6a494665a194e9cbb328d9496e
Bulovyatov <bam10112002@gmail.com>
2025-06-18 16:14:23 +0300

    -

--- src/components/NewAlbumBtn.jsx
+++ src/components/NewAlbumBtn.jsx
@@ -3,12 +3,14 @@ import { Plus } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const NewAlbumButton = () => (
-  <Button asChild>
-    <Link to="/new-album">
-      <Plus className="mr-2 h-4 w-4" />
-      Новый альбом
-    </Link>
-  </Button>
+  <div className="flex justify-center items-center w-full">
+    <Button asChild className="rounded-2xl">
+      <Link to="/new-album">
+        <Plus className="mr-2 h-4 w-4" />
+        Новый альбом
+      </Link>
+    </Button>
+  </div>
 );
 
 export default NewAlbumButton;
